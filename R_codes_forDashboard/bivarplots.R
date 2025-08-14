6673d24c52d259207f13516b5415c570d663b47d
profemagallanes-pucp <jmagallanes@pucp.edu.pe>
2025-08-13 23:58:38 -0300

    update

--- R_codes_forDashboard/bivarplots.R
+++ R_codes_forDashboard/bivarplots.R
@@ -8,39 +8,42 @@ library(ggrepel)
 
 ###
 link1="https://github.com/magallanes-at-UBA/DataViz/raw/main/"
-link2="fromPython/fragilityVariabilityGrowth_2023_2006.csv"
+link2="fromPython/fragilityGrowthVariability_HumRts.csv"
 
-vargrth =read.csv(paste0(link1,link2))
 
-###
-ggplot(data=vargrth) + geom_point(aes(x=p3_human_rights_growth,y=p3_human_rights_variability))
+volave =read.csv(paste0(link1,link2))
 
-###
-ggplot(data=vargrth) + geom_point(aes(x=p3_human_rights_growth,y=p3_human_rights_variability,color=region))
+names(volave)
 
 ###
-ggplot(data=vargrth) + geom_point(aes(x=p3_human_rights_growth,y=p3_human_rights_variability)) + facet_wrap(~region)
+ggplot(data=volave) + geom_point(aes(x=fragility.Growth_HumanRights,y=fragility.Volatility_HumanRights))
 
 ###
-table(vargrth$region)
+ggplot(data=volave) + geom_point(aes(x=fragility.Growth_HumanRights,y=fragility.Volatility_HumanRights,color=region))
+
+###
+ggplot(data=volave) + geom_point(aes(x=fragility.Growth_HumanRights,y=fragility.Volatility_HumanRights)) + facet_wrap(~region)
+
+###
+table(volave$region)
 americas=c('South America','Central America and the Caribbean','North America')
 
-ggplot(data=vargrth[vargrth$region %in% americas,],aes(x=p3_human_rights_growth,y=p3_human_rights_variability)) +
+ggplot(data=volave[volave$region %in% americas,],aes(x=fragility.Growth_HumanRights,y=fragility.Volatility_HumanRights)) +
     geom_text(aes(label=country,color=region)) + theme(legend.position = "bottom")
 
 ###
-ggplot(data=vargrth[vargrth$region %in% americas,],aes(x=p3_human_rights_growth,y=p3_human_rights_variability)) +
+ggplot(data=volave[volave$region %in% americas,],aes(x=fragility.Growth_HumanRights,y=fragility.Volatility_HumanRights)) +
     geom_text(aes(label=country),size=2) + facet_grid(region~.)
 
 ####
-bivar_americas=ggplot(data=vargrth[vargrth$region %in% americas,],aes(x=p3_human_rights_growth,y=p3_human_rights_variability)) + theme_light()+
+bivar_americas=ggplot(data=volave[volave$region %in% americas,],aes(x=fragility.Growth_HumanRights,y=fragility.Volatility_HumanRights)) + theme_light()+
     geom_label_repel(aes(label=country,color=region)) + theme(legend.position = "bottom") +
     geom_vline(xintercept = 0, color='grey') + scale_y_log10()
 bivar_americas
 
 ###
 farlands=c('South America','East and Southeast Asia')
-bivar_farlands=ggplot(data=vargrth[vargrth$region %in% farlands,],aes(x=p3_human_rights_growth,y=p3_human_rights_variability)) + theme_light()+
+bivar_farlands=ggplot(data=volave[volave$region %in% farlands,],aes(x=fragility.Growth_HumanRights,y=fragility.Volatility_HumanRights)) + theme_light()+
     geom_label_repel(aes(label=country,color=region,size=2),max.overlaps=20) + theme(legend.position = "bottom") +
     geom_vline(xintercept = 0, color='grey') + scale_y_log10()
 
