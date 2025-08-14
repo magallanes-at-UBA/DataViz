8ca33787554ca0779075d225e3d62dba2c2660eb
profemagallanes-pucp <jmagallanes@pucp.edu.pe>
2025-08-14 00:51:18 -0300

    updates

--- R_codes_forDashboard/multiplot.R
+++ R_codes_forDashboard/multiplot.R
@@ -2,6 +2,7 @@ rm(list = ls())
 setwd("~/Documents/GITHUBs/UBA/DataViz/R_codes_forDashboard")
 set.seed(123)  # For reproducibility
 
+
 ###
 library(sf)
 library(ggplot2)
@@ -13,13 +14,29 @@ link2="fromPython/fragilitymap.gpkg"
 
 border=sf::st_read(paste0(link1,link2),layer = "border")
 
-fragilityMap=sf::st_read(linkMap, layer = "countries")
+fragilityMap=sf::st_read(paste0(link1,link2), layer = "countries")
 
 head(fragilityMap)
 
+##
+ggplot() + theme_void() +
+geom_sf(data=border, color='grey') + 
+    geom_sf(data=fragilityMap, aes(fill=cluster_label)) 
 
+###
+ggplot() + theme_void() +
+geom_sf(data=border, color='grey') + 
+    geom_sf(data=fragilityMap, aes(fill=cluster_label)) + 
+      theme(legend.position = "bottom")
+
+
+###
 mapClus=ggplot() + theme_void() +
-geom_sf(data=border, color='grey') + geom_sf(data=fragilityMap, aes(fill=cluster_label))
+    geom_sf(data=border, color='grey') + 
+    geom_sf(data=fragilityMap, aes(fill=cluster_label)) + 
+    scale_fill_viridis_d(name = NULL) +
+    theme(legend.position = "bottom")
 
 mapClus
+
 saveRDS(mapClus,file = 'dashboards/mapClus.rds')
